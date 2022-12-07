b1dc61e34eefe96e4524d7ec55998047cb7e864d
Alphabarrie98 <barriealpha24@gmail.com>
2022-12-07 14:35:16 +0000

    Register

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Register from './components/Register';
 
 
 function App() {
   return (
     <div>
-      <Navbar />  
+      <Navbar /> 
+      <Register />
     </div>   
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -42,6 +42,43 @@ export default function Register() {
           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
           aria-describedby="emailHelp124" placeholder="Last name"/>
       </div>
+
+      <div class="form-group mb-6">
+        <input type="text" class="form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
+          aria-describedby="emailHelp124" placeholder="School of Choice"/>
+      </div>
+      <div class="form-group mb-6">
+        <input type="text" class="form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
+          aria-describedby="emailHelp124" placeholder="Grade of Student"/>
+      </div>
     </div>
     <div class="form-group mb-6">
       <input type="email" class="form-control block
@@ -77,6 +114,36 @@ export default function Register() {
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
         placeholder="Password"/>
     </div>
+
+    <div>
+    <table class="table-auto">
+  <thead>
+    <tr>
+      <th>School of your Choice</th>
+      <th>Location</th>
+      <th>popularity</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>  John lewis methodist</td>
+      <td>  Paynesville</td>
+      <td>  ****</td>
+    </tr>
+    <tr>
+      <td>  JJ. Robert</td>
+      <td>  12 street</td>
+      <td>  *****</td>
+    </tr>
+    <tr>
+      <td>  PCS</td>
+      <td>  Paynesville</td>
+      <td>  ***</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+
     <div class="form-group form-check text-center mb-6">
       <input type="checkbox"
         class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
